81bce756b5fac12d8d208970d218dd18cf4f2ef6
vikaszarpalagit config --global user.email vikaszarpala@gmail.com <vikaszarpala@gmail.com>
2026-08-21 23:13:40 +0530

    Update code

--- day1 variables & data types.py
+++ day1 variables & data types.py
@@ -1,6 +1,23 @@
-name = "vikas"
+# name = "vikas"
+# age = -21
+# price = 100.50
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# name1 = "v"
+# name2 = 'i'
+# name3 = '''k'''
+
+
+# print(name1)
+# print(name2)
+# print(name3)
+
 age = 21
-price = 100.50
+old = False
+a = None
 
 print(age)
-Result: 21
+print(old)
