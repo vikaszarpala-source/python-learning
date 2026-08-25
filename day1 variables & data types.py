0461084c0c4ffff263521e5e93fa8c57f7c516ce
vikaszarpalagit config --global user.email vikaszarpala@gmail.com <vikaszarpala@gmail.com>
2026-08-25 23:38:03 +0530

    added project

--- day1 variables & data types.py
+++ day1 variables & data types.py
@@ -1,68 +1,7 @@
-# name = "vikas"
-# age = -21
-# price = 100.50
+#WAP program to input 2 numbers & print their sum.and
 
-# print(type(name))
-# print(type(age))
-# print(type(price))
+first = int(input("enter first : "))
+secound = int(input("enter secound : "))
 
-# name1 = "v"
-# name2 = 'i'
-# name3 = '''k'''
+print("sum =", first + secound )
 
-
-# print(name1)
-# print(name2)
-# print(name3)
-
-# age = 21
-# old = False
-# a = None
-
-# print(age)
-# print(old)
-
-# a = 1000 
-# b = 500
-# diff = a - b
-# print(diff)
-
-# print("hello world")
-# """
-# # print("hello world") comment out. 
-# multi-line comment 
-# """
-
-# arithmetic operators 
-# a = 5 
-# b = 2
-
-
-# print(a+b)
-# print(a-b)
-# print(a*b)
-# print(a/b)
-# print(a//b)
-
-# # relational operators 
-# a = 50 
-# b = 20 
-
-# print(a == b) 
-# #false 
-
-# a = 50 
-# b = 20
-
-# print(a == b) #false
-# print(a != b) #true
-# print (a >= b) #true
-# print(a <= b) #false
-# print(a < b)#false
- 
-# num = 10
-# num =+ 10
-# print("num :", num)
-
-print(not False)
-print(not True)
