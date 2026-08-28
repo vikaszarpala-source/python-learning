4af31e4e7e65a02432e3ae536fb311fbd13c3e8b
vikaszarpalagit config --global user.email vikaszarpala@gmail.com <vikaszarpala@gmail.com>
2026-08-28 23:44:22 +0530

    add python string

--- day2 string.py
+++ day2 string.py
@@ -1,13 +1,6 @@
-text = input("Enter a string: ")
+name = "Vikas"
+city = "Pune"
 
-for char in set(text):
-    print(char, "=", text.count(char))
+username = name.lower() + "_" + city.lower()
 
-
-    text = input("Enter a string: ")
-
-characters = set(text)
-most_repeated = max(characters, key=text.count)
-
-print("Most repeated character:", most_repeated)
-print("Count:", text.count(most_repeated))
+print("Username:", username)
